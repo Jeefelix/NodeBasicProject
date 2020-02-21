33881c6e2842e61324650703d338df674df35956
Jeefelix <jessica.felix@ciclic.com.br>
2020-02-21 17:58:24 -0300

    refactoring timers

--- timers.js
+++ timers.js
@@ -6,7 +6,10 @@ let currentTime = 0;
 
 const incTime = () => {
     currentTime += waitInterval;
-    console.log(`waiting ${currentTime/1000} seconds`);
+    const p = Math.floor((currentTime/ waitTime)* 100);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`waiting....${p}%`)
 };
 
 console.log(`setting a ${waitTime/1000} second delay`);
