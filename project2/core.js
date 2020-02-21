eedcf92d00762fc1f07b1945dd2d547a4b64712b
Jeefelix <jessica.felix@ciclic.com.br>
2020-02-21 19:08:33 -0300

    in progress

---
+++ project2/core.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const util = require("util");
+
+util.log(path.basename(__filename));
+
+util.log("the name of the current file ");
+
+// const dirUploads = path.join(__dirname, "www","files", "uploads");
+// console.log(dirUploads);
+
+
